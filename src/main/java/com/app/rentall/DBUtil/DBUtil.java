4ed44ed7160e40b282df009a09f56f67eaed2441
Bhagat, Shubham Narendra <snbhagat@iu.edu>
2021-12-02 21:40:29 -0500

    chat messaging initial version

--- src/main/java/com/app/rentall/DBUtil/DBUtil.java
+++ src/main/java/com/app/rentall/DBUtil/DBUtil.java
@@ -9,18 +9,18 @@ public class DBUtil {
         Connection con = null;
         try {
             //Driver class loader
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String dbUrl = "jdbc:mysql://localhost:3306/rentall";
-            String username = "root";
-            String password = "Shivoham@8089";
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            String dbUrl = "jdbc:mysql://localhost:3306/rentall";
+//            String username = "root";
+//            String password = "Shivoham@8089";
 //            System.out.println(System.getenv("CLEARDB_DATABASE_URL"));
 
 
-//            URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-//            System.out.println("dburl"+dbUri);
-//            String username = dbUri.getUserInfo().split(":")[0];
-//            String password = dbUri.getUserInfo().split(":")[1];
-//            String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+            URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+            System.out.println("dburl"+dbUri);
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
 
             //create connection
                 con = DriverManager.getConnection(dbUrl, username, password);
